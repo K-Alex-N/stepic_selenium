982a4900eae1c0577da254b2f32c277b99c26735
Алексей <KurochkinAlexei@yandex.ru>
2024-07-13 22:33:41 +0200

    new lessons done

--- Selenium_course/3_4_6_test.py
+++ Selenium_course/3_4_6_test.py
@@ -1,16 +1,9 @@
 import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-@pytest.fixture(scope="class")
-def browser():
-    browser = webdriver.Chrome()
-    browser.get(link)
-    yield browser
-    browser.quit()
 
 @pytest.fixture(autouse=True)
 def prepare_data():
@@ -19,7 +12,10 @@ def prepare_data():
 
 class TestMainPage1():
     def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
 
+
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
